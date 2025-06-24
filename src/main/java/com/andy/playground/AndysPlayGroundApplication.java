1b92dae28ac2f1b34bf0cd3e410aaa550ebdb0be
megait <megait.com>
2025-06-24 18:54:54 +0900

    [신윤식] - 새로운 프로젝트

---
+++ src/main/java/com/andy/playground/AndysPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package com.andy.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndysPlayGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndysPlayGroundApplication.class, args);
+	}
+
+}
