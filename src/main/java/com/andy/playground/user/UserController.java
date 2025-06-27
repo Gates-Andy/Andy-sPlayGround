b0511afd5316e2edac8d6f375db27c191012c950
andy <mujish1@naver.com>
2025-06-27 16:42:34 +0900

    [신윤식] -20250627

--- src/main/java/com/andy/playground/user/UserController.java
+++ src/main/java/com/andy/playground/user/UserController.java
@@ -9,22 +9,30 @@ public class UserController {
 	
 	@GetMapping("/join/view")
 	public String joinInput() {
+		
 		return "user/join";
+		
 	}  //post
 	
 	@GetMapping("/duplicate-id")
 	public String duplicateIdInput() {
+		
 		return "user/duplicate";
+		
 	}
 	
 	@GetMapping("/login/view")
 	public String loginInput() {
+		
 		return "user/login";
+		
 	}  //post
 	
 	@GetMapping("/post/create")
 	public String createTimeline() {
+		
 		return "user/timeline";
+		
 	}  //post
 	
 }
