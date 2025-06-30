cd3e911443f1d3763241a690da92cac0c0a7500c
andy <mujish1@naver.com>
2025-06-30 18:06:19 +0900

    [신윤식] -2025 06 30 css정리 및 수정

--- src/main/java/com/andy/playground/user/UserRestController.java
+++ src/main/java/com/andy/playground/user/UserRestController.java
@@ -18,76 +18,51 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/user")
 @RestController
 public class UserRestController {
-
 	private final UserService userService;
 
 	public UserRestController(UserService userService) {
-
 		this.userService = userService;
-
 	}
 
 	// 1.회원가입
 	@PostMapping("/join")
 	public Map<String, String> join(@RequestParam("loginId") String loginId, @RequestParam("password") String password,
 			@RequestParam("name") String name, @RequestParam("email") String email) {
-
 		Map<String, String> resultMap = new HashMap<>();
-
 		if (userService.addUser(loginId, password, name, email)) {
-
 			resultMap.put("result", "success");
-
 		} else {
-
 			resultMap.put("result", "fail");
-
 		}
-
 		return resultMap;
-
 	}
 
 	// 2. 중복확인 아이디와 이메일
 	@GetMapping("/duplicate-id")
 	public Map<String, Boolean> isDuplicateId(@RequestParam("loginId") String loginId) {
-
 		Map<String, Boolean> resultMap = new HashMap<>();
-
 		if (userService.isDuplicateId(loginId)) {
-			
 			resultMap.put("isDuplicate", true);
-			
 		} else {
-			
 			resultMap.put("isDuplicate", false);
-			
 		}
-
 		return resultMap;
-
 	}
 
 	// 2.로그인
 	@PostMapping("/login")
 	public Map<String, String> login(@RequestParam("loginId") String loginId, @RequestParam("password") String password,
 			HttpServletRequest request) {
-
 		Map<String, String> resultMap = new HashMap<>();
-
 		User user = userService.getUser(loginId, password);
-
 		if (user != null) {
 			resultMap.put("result", "success");
 			HttpSession session = request.getSession();
-
 			session.setAttribute("userId", user.getId());
-			session.setAttribute("userName", user.getloginId()); // 로그인 한 사람
-			
+			session.setAttribute("userName", user.getloginId()); // 로그인 한 사람의 정보를 얻어 저장한다.(이 과정이 있어야 나중에 사용 할 수 있음)
 		} else {
 			resultMap.put("result", "fail");
 		}
 		return resultMap;
 	}
-
 }
