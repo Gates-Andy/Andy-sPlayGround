215218448fb9f07de92af919949d6ea0b2bd3b11
Andy <Andy.com>
2025-08-04 13:37:20 +0900

    [신윤식] -20250804 file 사진 실험

--- src/main/java/com/andy/playground/common/FileManager.java
+++ src/main/java/com/andy/playground/common/FileManager.java
@@ -10,8 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 public class FileManager {
 
-	public static final String FILE_UPLOAD_PATH = "D:\\ANDY_GATES\\WEB\\20250220\\springProject\\sample";
-	// public static final String FILE_UPLOAD_PATH = "C:\\Users\\mujik\\OneDrive\\바탕 화면\\project\\upload";
+	// public static final String FILE_UPLOAD_PATH = "D:\\ANDY_GATES\\WEB\\20250220\\springProject\\sample";
+	public static final String FILE_UPLOAD_PATH = "C:\\Users\\mujik\\OneDrive\\바탕 화면\\project\\upload";
 	
 	public static String saveFile(long userId, MultipartFile file) {
 		
